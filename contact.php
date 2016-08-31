a644369df0410463204f79bf93210c24facab073
Kristin Corona <kristincorona@gmail.com>
2016-08-31 15:38:11 -0400

    Update index; style; contact info

--- contact.php
+++ contact.php
@@ -38,7 +38,7 @@ if(get_magic_quotes_gpc()) {
 // Example $address = "joe.doe@yourdomain.com";
 
 //$address = "example@themeforest.net";
-$address = "example@themeforest.net";
+$address = "kristin@tedxlesley.com";
 
 
 // Configuration option.
